d570cad7a448f2f9a2236894da582be4b4036ec3
rohitsalem <rohitsalem@gmail.com>
2017-10-09 12:05:13 -0700

    update setup.sh

--- setup.sh
+++ setup.sh
@@ -5,7 +5,7 @@
 ## Purpose: setups the system for Ubuntu 16.04
 ## Installs: Terminator, Atom, Qtcreator, Virtual env, htop, ROS-Kinetic, docker-ce, git-gui,  
 
-sudo apt update
+sudo apt update 
 
 echo "$(tput setaf 2)Installing terminator(tput sgr0)$"
 sudo apt install terminator -y
@@ -25,35 +25,35 @@ echo "$(tput setaf 6) Installing virtual env: (tput sgr0)$"
 sudo apt install python-virtualenv -y
 sudo apt install python-pip -y 
 
-echo "$((tput setaf 2) Installing git-gui: (tput sgr0)$"
+echo "$(tput setaf 2) Installing git-gui: (tput sgr0)$"
 sudo apt install git-gui -y
 
 echo "$(tput setaf 3) Installing ROS :(tput sgr0)$"
 echo "$(tput setaf 2) Installing ROS : Setting up sources.list(tput sgr0)$"
 sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list'
 echo "$(tput setaf 2) Installing ROS : Setting up the keys (tput sgr0)$"
-sudo apt-key adv --keyserver hkp://ha.pool.sks-keyservers.net:80 --recv-key 421C365BD9FF1F717815A3895523BAEEB01FA116
-sudo apt-get update
+sudo apt-key adv --keyserver hkp://ha.pool.sks-keyservers.net:80 --recv-key 421C365BD9FF1F717815A3895523BAEEB01FA116 -y
+sudo apt-get update -y
 echo "$(tput setaf 2) Installing ROS Kinetic Desktop-full:(tput sgr0)$"
-sudo apt-get install ros-kinetic-desktop-full
+sudo apt-get install ros-kinetic-desktop-full -y
 echo "$(tput setaf 3) Initilizing ros dep: (tput sgr0)$"
-sudo rosdep init
+sudo rosdep init -y
 rosdep update
 echo "$(tput setaf 3)Installing ROS: Setting up enironment$"
 echo "source /opt/ros/kinetic/setup.bash" >> ~/.bashrc 
 source ~/.bashrc
 echo "$(tput setaf 5) Getting ros install (tput sgr0)$"
-sudo apt-get install python-rosinstall
+sudo apt-get install python-rosinstall -y
 
 echo "$(tput setaf 7) Installing Docker-CE: (tput sgr0)$"
 sudo apt-get install -y\
     apt-transport-https \
-    ca-certificates \
-    curl \
-    software-properties-common
+   ca-certificates \
+   curl \
+   software-properties-common
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 sudo add-apt-repository -y \
-   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+"deb [arch=amd64] https://download.docker.com/linux/$(. /etc/os-release; echo "$ID") \
    $(lsb_release -cs) \
    stable"
 sudo apt-get update
